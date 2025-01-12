17ca337f8eb774fc8a7ba81ab5f99dac9f1e9fc8
Vidit-Singla <singlavidit2428@gmail.com>
2025-01-12 19:47:22 +0530

    Add files via upload

---
+++ Even_or_Odd.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    int fact = 1;
+    while (n!=0) {
+        fact  = fact * n;
+        n -= 1;
+    }
+    cout<< fact;
+    return 0;   
+}
