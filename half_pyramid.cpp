faa3cf7cee9aa441c0b9b7e1713c553f854b36b9
Vidit-Singla <singlavidit2428@gmail.com>
2025-01-15 22:51:45 +0530

    Add files via upload

---
+++ half_pyramid.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    for (int row=0; row<n; row++) {
+        for (int col=0; col<row+1; col++) {
+            cout << "*";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
+    
+    
+
+    
+
+
