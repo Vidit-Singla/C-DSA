5e765bebc2346faf85da89871b0fdef3f59d3f64
Vidit-Singla <singlavidit2428@gmail.com>
2025-07-28 23:14:44 +0530

    Add files via upload

---
+++ jump_game.cpp
@@ -0,0 +1,21 @@
+#include<vector>
+class Solution {
+public:
+
+    bool solve(vector<int>& nums, int i){
+        if(i==nums.size()-1) return true;
+        if(i>=nums.size()) return false;
+        if(nums[i] == 0) return false;
+        bool reAns = false;
+        for (int jump = 1; jump <= nums[i]; jump++) {
+            reAns = reAns || solve(nums,i+jump);
+        }
+        return reAns;
+    }
+
+    bool canJump(vector<int>& nums) {
+        return solve(nums,0);
+
+        
+    }
+};
