a9a0798f6fea76c549dac288db6b37c0564447a6
Vidit-Singla <singlavidit2428@gmail.com>
2026-06-18 23:50:45 +0530

    Add files via upload

--- Square_Root.cpp
+++ Square_Root.cpp
@@ -1,50 +1,37 @@
 #include <iostream>
-#include<map>
-#include<vector>
-#include<algorithm>
-#include<unordered_map>
 using namespace std;
-
-double mySqrt(int x) {
-        int s = 0;
-        int e = x;
-        int ans = -1;
-        while (s <= e) { 
-            long long int mid = s + (e - s) / 2;
-            long long int product = mid * mid;
-            if (product == x) {
-                return mid;
-            } 
-            else if (product < x) {
-                ans = mid;
-                s = mid + 1;
-            } 
-            else {
-                e = mid - 1;
-            }
-        }
-        double sqrtans = (double) ans;
-        int precision = 1;
-        int factor = 1;
-        for (int i=0; i<precision; i++) {
-            factor = factor /10.0;
-            for (int n=0; n<=9; n++) {
-                int toAdd = factor * n;
-                if (sqrtans + toAdd < x) {
-                    sqrtans += toAdd;
-                }
-                else {
-                    break;
-                }
-
-            }
-        }
-    }
 
 int main() {
     int num = 54;
-    double ans = mySqrt(num);
-    cout << ans;
-    
+    int s = 0, e = num;
+    double ans = -1;
 
+    while (s <= e) {
+        int mid = s + (e - s) / 2;
+        if (mid * mid == num) {
+            ans = mid;
+            cout << ans; 
+            return 0;
+        } 
+        else if (mid * mid < num) {
+            ans = mid; 
+            s = mid + 1;
+        } 
+        else {
+            e = mid - 1;
+        }
+    }
+
+    double factor = 0.1;
+    int precision = 5;
+    
+    for (int i = 0; i < precision; i++) {
+        while ((ans + factor) * (ans + factor) <= num) {
+            ans += factor;
+        }
+        factor /= 10;
+    }
+
+    cout << ans; 
+    return 0;
 }
